466a21293a8cb9c1226732dff939937a83ce39e1
DanielGiraldiMichels <danigmichels@gmail.com>
2023-10-28 15:48:43 -0300

    Botão atualizar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const form = document.querySelector("form")
 const insert = document.querySelector("#btn-insert")
 const list = document.querySelector("#btn-list")
 const del = document.querySelector("#btn-del")
+const atualizar = document.querySelector("#btn-atualizar")
 
 const btnInsert = document.querySelector("#insertSubmit")
 
@@ -17,6 +18,9 @@ list.addEventListener("click", function(){
 
     list.classList.add("bg-primary")
     list.classList.add("text-white")
+
+    atualizar.classList.remove("bg-primary")
+    atualizar.classList.remove("text-white")
     
     let lista = ""
 
@@ -70,6 +74,9 @@ insert.addEventListener("click", function(){
     list.classList.remove("bg-primary")
     list.classList.remove("text-white")
 
+    atualizar.classList.remove("bg-primary")
+    atualizar.classList.remove("text-white")
+
     conteudo.innerHTML = `
         <form action="" onsubmit="return false">
             <div class="input-group mt-4">
@@ -94,8 +101,7 @@ insert.addEventListener("click", function(){
             </div>
         </form>
         `
-
-    })
+})
     
     function inserindo (){
         const name = document.querySelector("#name")
@@ -118,7 +124,7 @@ insert.addEventListener("click", function(){
             .then(result => result.json())
             .then((res) => {
                 if(res.error){
-                    console.log(res.error.message);
+                    console.log(res.error.message)
                 }else{
                     // console.log(res.message);
                     alert("Inserido com sucesso")
@@ -129,6 +135,80 @@ insert.addEventListener("click", function(){
     
 
 
+//-----Função para atualizar-----
+atualizar.addEventListener("click", function(){
+    del.classList.remove("bg-primary")
+    del.classList.remove("text-white")
+
+    insert.classList.remove("bg-primary")
+    insert.classList.remove("text-white")
+
+    list.classList.remove("bg-primary")
+    list.classList.remove("text-white")
+
+    atualizar.classList.add("bg-primary")
+    atualizar.classList.add("text-white")
+
+    conteudo.innerHTML = `
+    <form action="" onsubmit="return false">
+        <div class="input-group mt-4">
+            <span class="input-group-text" id="inputGroup-sizing-sm">ID</span>
+            <input id="atualizarId" type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group mt-4">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Name</span>
+            <input id="atualizarName" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group mt-4">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Species</span>
+            <input id="atualizarSpecie" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group mt-4">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Color</span>
+            <input id="atualizarColor" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            </div>
+            <div class="input-group mt-4">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Size</span>
+            <input id="atualizarSize" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        
+        <div class=" d-md-flex justify-content-end m-4">
+            <button id="insertSubmit" class="btn btn-primary" type="button" onClick="atualizando()">Submit</button>
+        </div>
+    </form>`
+})
+
+function atualizando(){
+    const atualizarId = document.querySelector("#atualizarId")
+    const atualizarName = document.querySelector("#atualizarName")
+    const atualizarSpecie = document.querySelector("#atualizarSpecie")
+    const atualizarColor = document.querySelector("#atualizarColor")
+    const atualizarSize = document.querySelector("#atualizarSize")
+
+    fetch("http://cafepradev.com.br:21020/animals/update", {
+        method : "PUT",
+        headers : {
+            "Content-type" : "application/json; charset=UTF-8"
+        },
+        body : JSON.stringify({
+            id: atualizarId.value,
+            name: atualizarName.value,
+            species: atualizarSpecie.value,
+            color: atualizarColor.value,
+            size: atualizarSize.value
+        })
+    })
+    .then(result => result.json())
+            .then((res) => {
+                if(res.error){
+                    console.log(res.error.message)
+                }else{
+                    // console.log(res.message);
+                    alert("Atualizado com sucesso")
+                }
+            })
+            .catch(err => console.log(err))
+}
 
 //-----Funções para deletar-----
 del.addEventListener("click", function(){
@@ -140,6 +220,9 @@ del.addEventListener("click", function(){
 
     list.classList.remove("bg-primary")
     list.classList.remove("text-white")
+
+    atualizar.classList.remove("bg-primary")
+    atualizar.classList.remove("text-white")
     
     conteudo.innerHTML = `
         <form action="" onsubmit="return false">
